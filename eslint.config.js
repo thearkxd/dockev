c9d14bcf1ff64bf098f96137b4d561a61d7ace54
Theark <59927799+thearkxd@users.noreply.github.com>
2025-12-25 03:26:25 +0300

    eslint update

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    rules: {
-      // Modal açıldığında form state'ini resetlemek için useEffect içinde setState kullanımına izin ver
-      // Bu durumda modal açıldığında form'u resetlemek için gerekli
-      '@typescript-eslint/no-floating-promises': 'off',
-    },
-  },
-])
+	globalIgnores(["dist"]),
+	{
+		files: ["**/*.{ts,tsx}"],
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser
+		},
+		rules: {
+			"react-hooks/set-state-in-effect": "off",
+			"react-hooks/exhaustive-deps": "warn",
+			"@typescript-eslint/no-floating-promises": "off"
+		}
+	}
+]);
